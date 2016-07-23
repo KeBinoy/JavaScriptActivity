4779ca47cb5cecdf678ef47bc1eaf7704435e8de
myanne <anntejadillo@gmail.com>
2016-07-23 12:12:30 +0800

    First JavaScript Acitivity

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.getElementById('heading').innerHTML = "JavaScript Title Heading";
+document.getElementById('subHeading').innerHTML = "JavaScript Subtitle Heading";
+document.getElementById('tbl').innerHTML = "<tr><th>Name</th><th>Gender</th></tr>"+
+										"<tr><td>Reymar</td><td>Male</td></tr>"+
+										"<tr><td>Cindy</td><td>Female</td></tr>"+
+										"<tr><td>Kevin</td><td>Male</td></tr>";
